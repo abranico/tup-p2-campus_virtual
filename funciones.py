af11d4d4197e153e758b69e0985e275e61f0af16
abranico <abranico011@gmail.com>
2023-10-18 12:04:11 -0300

    Correccion de mostrar cursos de alumno y profesor

--- funciones.py
+++ funciones.py
@@ -1,4 +1,5 @@
-import random, string
+import random
+import string
 
 
 def ingresar_como_alumno(estudiantes):
@@ -16,48 +17,62 @@ def ingresar_como_alumno(estudiantes):
     else:
         print("Email incorrecto: Debe darse de alta en alumnado.")
 
+
 def ingresar_como_profesor(profesores):
     profesor_actual = None
-    email= input("Ingrese EMAIL: ")
-    password= input("Ingrese CONTRASEÑA: ")
+    email = input("Ingrese EMAIL: ")
+    password = input("Ingrese CONTRASEÑA: ")
     for profesor in profesores:
-        if email== profesor.email:
-            profesor_actual=profesor
+        if email == profesor.email:
+            profesor_actual = profesor
     if profesor_actual:
-        if profesor_actual.validar_credenciales(email,password):
+        if profesor_actual.validar_credenciales(email, password):
             return profesor_actual
         else:
             print("ERROR DE INGRESO")
     else:
         print("Email incorrecto: Debe darse de alta en alumnado.")
 
+
 def ver_cursos(cursos):
     for curso in cursos:
-        print(f"Materia: {curso}  Carrera: Tecnicatura Universitaria en Programación")
+        print(
+            f"Materia: {curso}  Carrera: Tecnicatura Universitaria en Programación")
+
 
 def nuevo_curso(cursos, Curso, profesor):
-    nombre_curso= input("Ingrese nombre del curso nuevo: ")
-    matriculacion = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
-    curso_nuevo=Curso(nombre_curso, matriculacion)
+    nombre_curso = input("Ingrese nombre del curso nuevo: ")
+    matriculacion = ''.join(random.choice(
+        string.ascii_letters + string.digits) for _ in range(5))
+    curso_nuevo = Curso(nombre_curso, matriculacion)
     cursos.append(curso_nuevo)
     profesor.dictar_curso(curso_nuevo)
     print("El curso se agrego exitosamente.")
     print(f"Nombre: {nombre_curso}")
     print(f"Contraseña: {matriculacion}")
 
+
 def profesor_mostrar_cursos(profesor):
     print("\n")
-    for indice,curso in enumerate(profesor.mis_cursos):
+    for indice, curso in enumerate(profesor.mis_cursos):
         print(f"{indice+1} - {curso}")
+    seleccion = int(input("Seleccione un curso: "))
+    if seleccion > 0 and seleccion <= len(profesor.mis_cursos):
+        for curso in profesor.mis_cursos:
+            if curso.nombre == profesor.mis_cursos[seleccion-1].nombre:
+                print(f"Nombre: {curso.nombre}")
+                print(f"Contraseña: {curso.contrasenia}")
+    else:
+        print("Curso inexistente.")
 
 
 def matricularse_alumno(cursos, alumno):
     print("\n")
     encontrado = False
-    for indice,curso in enumerate(cursos):
+    for indice, curso in enumerate(cursos):
         print(f"{indice+1} - {curso}")
-    
-    seleccion = int(input("Seleccione una opcion: "))
+
+    seleccion = int(input("Seleccione un curso: "))
     if seleccion > 0 and seleccion <= len(cursos):
         for curso in alumno.mis_cursos:
             if curso.nombre == cursos[seleccion-1].nombre:
@@ -72,12 +87,18 @@ def matricularse_alumno(cursos, alumno):
 
     else:
         print("Curso inexistente.")
-        
+
 
 def alumno_mostrar_curso(alumno):
     print("\n")
-    for indice,curso in enumerate(alumno.mis_cursos):
+    for indice, curso in enumerate(alumno.mis_cursos):
         print(f"{indice+1} - {curso}")
 
-
-
+    seleccion = int(input("Seleccione un curso: "))
+    if seleccion > 0 and seleccion <= len(alumno.mis_cursos):
+        for curso in alumno.mis_cursos:
+            if curso.nombre == alumno.mis_cursos[seleccion-1].nombre:
+                print(f"Nombre: {curso.nombre}")
+                print(f"Contraseña: {curso.contrasenia}")
+    else:
+        print("Curso inexistente.")
